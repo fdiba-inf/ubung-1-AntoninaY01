b8dc66864c06eefaf3288d5d6b303a7e657b1d22
AntoninaY01 <a.yordanova01@icloud.com>
2020-09-30 14:13:02 +0000

    Uebung1

---
+++ src/exercise1/tree.java
@@ -0,0 +1,12 @@
+package exercise1;
+
+public class tree{
+  public static void main (String[] args){
+System.out.print("    *\n");
+System.out.print("   ***\n");
+System.out.print("  *****\n");
+System.out.print(" *******\n");
+System.out.print("*********\n");
+System.out.print("   ***\n");
+  }
+}
